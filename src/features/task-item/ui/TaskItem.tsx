aab965b10172cedc630fb53fffa695bef77f80a8
Smail000 <shibalenkov.kir@gmail.com>
2025-07-25 22:44:31 +0300

    added TaskItem

---
+++ src/features/task-item/ui/TaskItem.tsx
@@ -0,0 +1,28 @@
+import type { CardCategory, CardPriority, CardStatus } from "@/entities/task/model/taskState"
+import { Chip } from "@/entities/task/ui"
+import { IconButton } from "@/shared/ui/button"
+import { Layout } from "@/shared/ui/layout"
+import { Text, Title } from "@/shared/ui/typography"
+
+type TaskItemProps = {
+    id: number
+    title: string
+    discription?: string
+    chipList: [ CardStatus, CardCategory, CardPriority ]
+    onEdit: (id: number) => void
+}
+
+export default function TaskItem({ id, title, discription, chipList, onEdit }: TaskItemProps) {
+    return (
+        <Layout as="div" color="light" direction="column" padding="large" gap="medium" className="max-w-[420px] w-full">
+            <Title size="large" tag="h3" weight="semibold">{ title }</Title>
+            { discription ? (<Text size="base" tag="p" weight="medium">{ discription }</Text>) : <></> }
+            <div className="flex flex-row flex-wrap gap-[8px]">
+                <Chip as={chipList[0]}/>
+                <Chip as={chipList[1]}/>
+                <Chip as={chipList[2]}/>
+            </div>
+            <IconButton as="Edit" bgcolor="dark" onClick={() => onEdit(id)}/>
+        </Layout>
+    )
+}
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,6 @@
 
 import { Chip } from "@/entities/task/ui";
+import { TaskItem } from "@/features/task-item/ui";
 import { useResetKey } from "@/shared/lib/hooks";
 import { IconButton } from "@/shared/ui/button";
 import { Dropdown } from "@/shared/ui/dropdown";
@@ -13,7 +14,7 @@ export function HomePage() {
     const { key: inputKey, trigger: inputReset } = useResetKey(); // Для Input
 
     return (
-        <>
+        <div className="p-[20px] flex flex-col gap-[10px]">
             <Title
                 tag="h1"
                 weight="semibold"
@@ -37,7 +38,14 @@ export function HomePage() {
                 <Chip as="In Progress" />
                 <Input icon="Search" placeholder="Введите текст..." resetKey={inputKey}/>
             </Layout>
-        </>
+            <TaskItem
+                id={1}
+                title="Documentation for some app"
+                discription="Тут находиться некоторая текст-рыба для описания задачи, но проблема в том, что описание не несет никакой смысловой нагрузки в данном случае, а вообще она должна быть."
+                chipList={[ "In Progress", "Documentation", "Medium" ]}
+                onEdit={() => {console.log("Cliked!")}}
+            />
+        </div>
     )
 }
 // <div className="flex flex-row p-[10px] gap-[10px]">
