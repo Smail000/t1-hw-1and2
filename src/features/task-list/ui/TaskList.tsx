ab895638b575791703cf6070eb2b1c1cad64c63e
Smail000 <shibalenkov.kir@gmail.com>
2025-07-25 23:20:12 +0300

    added TaskList

---
+++ src/features/task-list/ui/TaskList.tsx
@@ -0,0 +1,20 @@
+import type { Task } from "@/entities/task/model/taskState"
+import { TaskItem } from "@/features/task-item/ui"
+
+type TaskListProps = {
+    tasks: Task[]
+    onEdit: (id: number) => void
+}
+
+export default function TaskList({ tasks, onEdit }: TaskListProps) {
+    return (
+        <div className="flex flex-row flex-wrap gap-[20px]">
+            { tasks.map(task => 
+                    <TaskItem id={task.id} title={task.title} discription={task.description}
+                    chipList={[ task.tags.status, task.tags.category, task.tags.priority ]}
+                    onEdit={onEdit}/>
+                )
+            }
+        </div>
+    )
+}
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,70 @@
 
+import type { Task } from "@/entities/task/model/taskState";
 import { Chip } from "@/entities/task/ui";
-import { TaskItem } from "@/features/task-item/ui";
+import TaskList from "@/features/task-list/ui/TaskList";
 import { useResetKey } from "@/shared/lib/hooks";
 import { IconButton } from "@/shared/ui/button";
 import { Dropdown } from "@/shared/ui/dropdown";
 import { Input } from "@/shared/ui/input";
 import { Layout } from "@/shared/ui/layout";
 import { Title, Text } from "@/shared/ui/typography";
+
+const tasks: Task[] = [
+    {
+        id: 1,
+        title: "Реализовать JWT-авторизацию",
+        description: "Добавить вход/регистрацию с токенами",
+        tags: {
+            category: "Feature",
+            status: "In Progress",
+            priority: "High"
+        },
+        createdDate: 1719820800000
+    },
+    {
+        id: 2,
+        title: "Создать макет главной страницы",
+        tags: {
+            category: "Feature",
+            status: "ToDo",
+            priority: "Medium"
+        },
+        createdDate: 1719907200000
+    },
+    {
+        id: 6,
+        title: "Исправить потерю данных при редактировании",
+        description: "Баг #42: пропадает описание при сохранении",
+        tags: {
+            category: "Bug",
+            status: "ToDo",
+            priority: "High"
+        },
+        createdDate: 1720252800000 // 2024-07-06
+    },
+    {
+        id: 4,
+        title: "Написать тесты для эндпоинтов задач",
+        description: "Покрыть 100% кейсов для /api/tasks",
+        tags: {
+            category: "Test",
+            status: "Done",
+            priority: "Medium"
+        },
+        createdDate: 1719993600000 // 2024-07-03
+    },
+    {
+        id: 5,
+        title: "Обновить README.md",
+        description: "Добавить раздел по настройке среды",
+        tags: {
+            category: "Documentation",
+            status: "ToDo",
+            priority: "Low"
+        },
+        createdDate: 1720080000000
+    }
+]
 
 export function HomePage() {
 
@@ -25,7 +83,7 @@ export function HomePage() {
                 weight="medium"
                 size="base"
             >Тут находиться некоторая текст-рыба для описания задачи, но проблема в том, что описание не несет никакой смысловой нагрузки в данном случае, а вообще она должна быть.</Text>
-            <Layout direction="row" padding="base" gap="base" color="light" className="items-center">
+            <Layout direction="row" padding="base" gap="base" color="light" className="items-center flex-wrap">
                 <IconButton as="X" bgcolor="base" />
                 <IconButton as="Check" bgcolor="base" />
                 <IconButton as="ChevronDown" bgcolor="dark" />
@@ -34,17 +92,13 @@ export function HomePage() {
                     dropdownReset();
                     inputReset();
                 }} />
-                <Dropdown title="Приоритет" items={[ "Low", "Medium", "High" ]} className="min-w-[200px]"resetKey={dropdownKey}/>
+                <Dropdown title="Приоритет" items={["Low", "Medium", "High"]} className="min-w-[200px]" resetKey={dropdownKey} />
                 <Chip as="In Progress" />
-                <Input icon="Search" placeholder="Введите текст..." resetKey={inputKey}/>
+                <Input icon="Search" placeholder="Введите текст..." resetKey={inputKey} />
             </Layout>
-            <TaskItem
-                id={1}
-                title="Documentation for some app"
-                discription="Тут находиться некоторая текст-рыба для описания задачи, но проблема в том, что описание не несет никакой смысловой нагрузки в данном случае, а вообще она должна быть."
-                chipList={[ "In Progress", "Documentation", "Medium" ]}
-                onEdit={() => {console.log("Cliked!")}}
-            />
+            <TaskList tasks={tasks} onEdit={(id) => {
+                console.log(`Попытка редактировать задачу ${id}`);
+            }} />
         </div>
     )
 }
