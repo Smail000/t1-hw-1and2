08988ce93371d5d0106f60817aaddaeabccdaf09
Smail000 <shibalenkov.kir@gmail.com>
2025-07-25 16:33:26 +0300

    added layout

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,7 @@
 
 import { IconButton } from "@/shared/ui/button";
+import { Icon } from "@/shared/ui/icon";
+import { Layout } from "@/shared/ui/layout";
 import { Title, Text } from "@/shared/ui/typography";
 
 export function HomePage() {
@@ -15,13 +17,21 @@ export function HomePage() {
                 weight="medium"
                 size="base"
             >Тут находиться некоторая текст-рыба для описания задачи, но проблема в том, что описание не несет никакой смысловой нагрузки в данном случае, а вообще она должна быть.</Text>
-            <div className="flex flex-row p-[10px] gap-[10px]">
+            <Layout direction="row" padding="base" gap="base" color="light">
                 <IconButton as="X" bgcolor="base" />
                 <IconButton as="Check" bgcolor="base" />
                 <IconButton as="ChevronDown" bgcolor="dark" />
                 <IconButton as="ChevronUp" bgcolor="dark" />
                 <IconButton as="Search" bgcolor="dark" onClick={() => {console.log(1)}} />
-            </div>
+                <Layout direction="row" padding="base" color="dark" gap="base" doHover>
+                    <Text size="medium" tag="span" weight="medium" unselectible>Приоритет</Text>
+                    <Icon as="ChevronDown"/>
+                </Layout>
+            <Layout color="dark" padding="small">
+                <Text size="base" tag="span" weight="medium" unselectible>In Progress</Text>
+            </Layout>
+            </Layout>
         </>
     )
 }
+// <div className="flex flex-row p-[10px] gap-[10px]">
---
+++ src/shared/ui/layout/Layout.tsx
@@ -0,0 +1,57 @@
+
+const Direction = {
+    row: "flex-row",
+    column: "flex-col"
+}
+
+const Padding = {
+    small: "py-[8px] px-[12px]",
+    base: "py-[12px] px-[16px]",
+    medium: "p-[24px]",
+    large: "p-[40px]"
+}
+
+const Gap = {
+    none: "gap-[0px]",
+    base: "gap-[8px]",
+    medium: "gap-[16px]"
+}
+
+const Color = {
+    white: "bg-[color:#fff]",
+    light: "bg-[color:#E9F7FF]",
+    dark: "bg-[color:#C1DCFF]"
+}
+
+type As = "div" | "button"
+
+const Hover = {
+    white: "",
+    light: "hover:bg-[color:#94BED7]",
+    dark: "hover:bg-[color:#7DA0CA]"
+}
+
+type LayoutProps = {
+    children: React.ReactElement | React.ReactElement[] | string
+    direction?: keyof typeof Direction
+    padding?: keyof typeof Padding
+    gap?: keyof typeof Gap
+    color?: keyof typeof Color
+    as?: As
+    doHover?: boolean
+    onClick?: (React.MouseEventHandler<HTMLDivElement> & React.MouseEventHandler<HTMLButtonElement>)
+}
+
+export default function Layout({
+    children, direction="row", padding="base", gap="base", color="light", as="div", doHover=false, onClick
+}: LayoutProps) {
+    const Component = as;
+    return (
+        <Component className={`flex rounded-[16px] w-fit h-fit select-none` +
+            `${Direction[direction]} ${Padding[padding]} ${Gap[gap]} ${Color[color]} ${doHover && Hover[color]}`}
+            onClick={onClick}
+        >
+            { children }
+        </Component>
+    )
+}
