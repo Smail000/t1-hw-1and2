f7c33a1d5a05926c977954bb6b854f7f6ec0b3b4
Smail000 <shibalenkov.kir@gmail.com>
2025-07-25 23:19:55 +0300

    new entity Task

--- src/entities/task/model/taskState.ts
+++ src/entities/task/model/taskState.ts
@@ -2,3 +2,15 @@
 export type CardCategory = "Bug" | "Feature" | "Documentation" | "Refactor" | "Test"
 export type CardStatus = "ToDo" | "In Progress" | "Done"
 export type CardPriority = "Low" | "Medium" | "High"
+
+export type Task = {
+	id: number,
+	title: string,
+	description?: string,
+	tags: {
+		category: CardCategory,
+		status: CardStatus,
+		priority: CardPriority
+	},
+	createdDate: number // timestamp
+}
